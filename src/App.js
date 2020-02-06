94f566b43fb71e16c642648b13c0e53d455403fd
seungmin <min7490@naver.com>
2020-02-06 14:08:24 +0900

    실습

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Potato from './potato';
 
-class App extends Component {
-  render(){return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Potato />
     </div>
-  );
-}
+  )
 }
 
 export default App;
+
+
+/*
+
+./ : 같은 디렉토리를 의미한다.
+jsx 는 javascript안의 HTML 이다. component를 만들고 어떻게 사용하는 지에 대한 것
+
+applicatoin 안에서 많은 component를 넣을수 있다. abs그리고 이러한 component안에 더 많은 component를 import 할 수 있다.
+*/
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+<App /> :: 이것을 기본적으로 component 라고 부른다. 
+그리고 react는 component 와 함께 동작해 , 모든것은 component 다. 
+
+component ::  html을 반환하는 함수 
+react는 component를 사용해서 HTML 처럼 작성하려는 경우 에 필요하다.
+
+javascript 와 HTML 사이의 이러한 조합얼 JSX라고 부른다. 
+이건 react에서 나온 매우 custom한 유일한 개념이다. 리엑트에 특화된 개념중 하나.
+
+component를 만드는 방법
+1. src안에 새로운 파일을 만든다. potato.js
+2. 그리고 component를 작헝할 때 마다 import React from "react";를 써줘야만 한다.
+(이것을 하지 않으먼 react는 여기에 jsx가 있는 component를 사용 하는 것을 이해하지 못한다)
+
+
+React application이 하나의 component만을 rendering 한다.
+*/
